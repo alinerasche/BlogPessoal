e3118caa7e45aac84994931c06a5079828c64ca3
Aline <aline.rasche25@gmail.com>
2022-05-11 11:21:11 -0300

    Atualizações Security

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -3,34 +3,14 @@ package com.generation.blogpessoal.service;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
-import com.generation.blogpessoal.model.Usuario;
-import com.generation.blogpessoal.model.UsuarioLogin;
-import com.generation.blogpessoal.repository.UsuarioRepository;
-
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-/**
- *  A Classe UsuarioService implementa as regras de negócio do Recurso Usuario.
- *  
- *  Regras de negócio são as particularidades das funcionalidades a serem 
- *  implementadas no objeto, tais como:
- *  
- *  1) O Usuário não pode estar duplicado no Banco de dados
- *  2) A senha do Usuario deve ser criptografada
- *  
- *  Observe que toda a implementação dos metodos Cadastrar, Atualizar e 
- *  Logar estão implmentadas na classe de serviço, enquanto a Classe
- *  Controller se limitará a checar a resposta da requisição.
- */
-
- /**
- * A Anotação @Service indica que esta é uma Classe de Serviço, ou seja,
- * implementa todas regras de negócio do Recurso Usuário.
- */
-
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.model.UsuarioLogin;
+import com.generation.blogpessoal.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
